d39a8c09bcbd3cbc72dd485d8351b9cd94ddf095
TaDangDao <166279082+TaDangDao@users.noreply.github.com>
2025-09-14 20:07:15 +0700

    fix movement+ cam

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+﻿using Unity.Cinemachine;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -5,9 +6,19 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private InputSystem_Actions input;
     [SerializeField] private float rotation = 10f;
     [SerializeField] private float speed = 10f;
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float acceleration = 10f;   
+    [SerializeField] private float deceleration = 10f;   
+    private Vector3 currentVelocity; // dung cho SmoothDamp
+    private Vector2 moveDir;
     private Vector3 dir;
     private InteractableObj current_InteractableObj;
-
+    private Vector3 camForward;
+    private Vector3 camRight;
+    private Vector3 targetVelocity;
+    private Vector3 smoothVelocity;
+    private Vector3 flatVel;
     void Start()
     {
         input = new InputSystem_Actions();
@@ -28,14 +39,42 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector2 moveDir = input.Player.Move.ReadValue<Vector2>().normalized;
-        dir.x = moveDir.x;
-        dir.z = moveDir.y;
-        transform.position += dir * speed * Time.deltaTime;
-        transform.forward = Vector3.Slerp(transform.forward, dir, Time.deltaTime * rotation);
+        moveDir = input.Player.Move.ReadValue<Vector2>().normalized;
+        camForward = cameraTransform.forward;
+        camForward.y = 0;
+        camForward.Normalize();
+        camRight = cameraTransform.right;
+        camRight.y = 0;
+        camRight.Normalize();
+      
+        // Chuyen input WASD sang world space dua tren camera
+        dir = camForward * moveDir.y + camRight * moveDir.x;
+    }
+    private void FixedUpdate()
+    {
+        // Tinh van toc muc tieu
+        targetVelocity = dir * speed;
+
+        // Giu lai gravity
+        targetVelocity.y = rb.linearVelocity.y;
+
+        // Dung SmoothDamp de tang/giam toc
+        smoothVelocity = Vector3.SmoothDamp(rb.linearVelocity, targetVelocity, ref currentVelocity,
+            dir.sqrMagnitude > 0.01f ? 1f / acceleration : 1f / deceleration);
+
+        rb.linearVelocity = smoothVelocity;
+
+        // Xoay nhan vat theo huong di chuyen (neu co input)
+        flatVel.x = rb.linearVelocity.x;
+        flatVel.z = rb.linearVelocity.z;
+        flatVel.y = 0; 
+        if (flatVel.sqrMagnitude > 0.01f)
+        {
+            Quaternion targetRot = Quaternion.LookRotation(flatVel, Vector3.up);
+            rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRot, rotation * Time.fixedDeltaTime));
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
---
+++ Assets/Script/QuestSystem/QuestState.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public enum QuestState 
+{
+    REQUIREMENTS_NOT_MET=0,
+    CAN_START=10,
+    IN_PROGRESS=20,
+    CAN_FINISH=30,
+    FINISHED=40
+}
