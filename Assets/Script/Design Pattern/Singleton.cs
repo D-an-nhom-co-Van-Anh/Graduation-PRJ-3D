5da5ff9f19f26d57f417f2a7661a18930310c8f6
TaDangDao <166279082+TaDangDao@users.noreply.github.com>
2025-10-17 15:45:47 +0700

    Update Singleton.cs

--- Assets/Script/Design Pattern/Singleton.cs
+++ Assets/Script/Design Pattern/Singleton.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+public abstract class Singleton_GL<T> : MonoBehaviour where T : MonoBehaviour
 {
     public static T Instance { get; private set; }
     protected virtual void Awake() => Instance = this as T;
@@ -12,7 +12,7 @@ public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     }
 }
 
-public abstract class SingletonPersistent<T> : Singleton<T> where T : MonoBehaviour
+public abstract class SingletonPersistent<T> : Singleton_GL<T> where T : MonoBehaviour
 {
     protected override void Awake()
     {
