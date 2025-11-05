7bdfedb1ef6a9014f09af2aa8410cca2acff9d3f
TaDangDao <166279082+TaDangDao@users.noreply.github.com>
2025-11-05 22:19:49 +0700

    cmt

--- Assets/Script/ShopItem.cs
+++ Assets/Script/ShopItem.cs
@@ -1,10 +1,23 @@
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
-
+using TMPro;
 public class ShopItem : MonoBehaviour
 {
     [SerializeField]private float cost;
     [SerializeField]private float value;
+    [SerializeField] private TextMeshProUGUI price;
     public float Cost => cost;
     public float Value => value;
+    private void Awake()
+    {
+        price.SetText(cost.ToString());
+    }
+    public void ShowSelectedUI()
+    {
+
+    }
+    public void HideSelectedUI()
+    {
+
+    }
 }
