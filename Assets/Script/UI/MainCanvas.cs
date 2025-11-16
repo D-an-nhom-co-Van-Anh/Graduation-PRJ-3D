48f9c0cb22db97c7bdd158815bb5947eeec69507
TaDangDao <166279082+TaDangDao@users.noreply.github.com>
2025-11-16 20:03:24 +0700

    fix quest + them UI cho Quest Table

--- Assets/Script/UI/MainCanvas.cs
+++ Assets/Script/UI/MainCanvas.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,7 +9,8 @@ public class MainCanvas : UICanvas
 {
     [SerializeField] private Button missionButton;
     [SerializeField] private GameObject missionBoard;
-    [SerializeField] private List<TextMeshProUGUI> questState;
+    [SerializeField] private List<QuestUI> questUI;
+    private string questState;
     private Dictionary<string, Quest> questMap;
     private void Awake()
     {
@@ -16,6 +18,7 @@ public class MainCanvas : UICanvas
 
             missionBoard.SetActive(true);
             UpdateMissionBoard();
+            Cursor.visible = true;
         });
     }
     public void UpdateMissionBoard()
@@ -24,8 +27,41 @@ public class MainCanvas : UICanvas
         int i = 0;
         foreach(Quest q in questMap.Values)
         {
-            questState[i].SetText(q.state.ToString());
+            if (i < questUI.Count)
+            {
+                if (q.state == QuestState.REQUIREMENTS_NOT_MET)
+                {
+                    questState = "        X ";
+                }
+                else
+                {
+                    questState = q.state.ToString();
+                }
+                questUI[i].SetInfo(q.info.id.ToString(),questState,q.info.displayName);
+            }
             i++;
         }
+        if (i < questUI.Count)
+        {
+            for (int j = i; j < questUI.Count; j++)
+            {
+                Destroy(questUI[i].gameObject);
+            }
+        }
+                  
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M)){
+            
+            missionBoard.SetActive(true);
+            UpdateMissionBoard();
+            Cursor.visible = true;
+        }
+    }
+    public void CloseQuestUI()
+    {
+        missionBoard.SetActive(false);
+        Cursor.visible = false;
     }
 }
