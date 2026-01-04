ddbe9de7cc96301052395f36ae1b24226eec7db9
Nguyễn giang Linh <126006181+myLovetory@users.noreply.github.com>
2026-01-04 19:21:42 +0700

    update tutorial CS

--- Assets/Script/UI/Tutorial UI.cs
+++ Assets/Script/UI/Tutorial UI.cs
@@ -1,16 +1,27 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class TutorialUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject tutorial_Image;
     void Start()
     {
-        
+        // Ẩn UI khi bắt đầu game
+        tutorial_Image.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Nhấn Tab để bật / tắt UI
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ToggleTutorial();
+        }
+    }
+
+    // tab + button
+    public void ToggleTutorial()
+    {
+        bool isActive = tutorial_Image.activeSelf;
+        tutorial_Image.SetActive(!isActive);
     }
 }
